9eff8658e6a0a463dff4cb538535b8df753faa11
philE22 <moonwhlking@gmail.com>
2023-01-16 22:13:15 +0900

    First commit

---
+++ src/main/java/com/project/oneyearfourcutrefactoring/OneyearfourcutRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.project.oneyearfourcutrefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneyearfourcutRefactoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneyearfourcutRefactoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/oneyearfourcutrefactoring/OneyearfourcutRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.oneyearfourcutrefactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneyearfourcutRefactoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
